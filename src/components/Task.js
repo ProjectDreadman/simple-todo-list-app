4b1a5290fbc95854af2be840d9cc3b3d9ad877a9
ProjectDreadman <168795340+ProjectDreadman@users.noreply.github.com>
2024-06-15 19:09:52 -0400

    Create Task.js

---
+++ src/components/Task.js
@@ -0,0 +1,25 @@
+
+### Basic Component Structure (React Example)
+
+#### `src/components/Task.js`
+```javascript
+import React from 'react';
+
+const Task = ({ task, toggleComplete, removeTask, editTask }) => {
+  return (
+    <div className="task">
+      <input 
+        type="checkbox" 
+        checked={task.completed} 
+        onChange={() => toggleComplete(task.id)} 
+      />
+      <span style={{ textDecoration: task.completed ? 'line-through' : '' }}>
+        {task.text}
+      </span>
+      <button onClick={() => editTask(task.id)}>Edit</button>
+      <button onClick={() => removeTask(task.id)}>Delete</button>
+    </div>
+  );
+};
+
+export default Task;
